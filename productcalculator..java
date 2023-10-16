ebc088edb225a554b9bf81fe33635921b4842255
cherreesims <145623229+cherreesims@users.noreply.github.com>
2023-10-15 22:21:02 -0500

    Update productcalculator..java

--- productcalculator..java
+++ productcalculator..java
@@ -1,1 +1,25 @@
+import java.util.Scanner;
 
+public class RecursiveCalculator {
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    double[] numbers = new double[5];
+
+    for (int i = 0; i < 5; i++) {
+      System.out.print("Enter number " + (i+1) + "; ");
+      numbers[i] = scanner.nextDouble();
+    }
+
+    double product = calculateProduct(numbers, 0);
+    System.out.println("The product of five numbers is: " + product)L
+ }
+
+  public static double calculateProduct(double[] numbers, int, int index) {
+    (index == 4) {
+      return numbers[index];
+    else {
+      return numbers[index] * calculateProduct(numbers, index +1);
+    }
+    }
+  }
